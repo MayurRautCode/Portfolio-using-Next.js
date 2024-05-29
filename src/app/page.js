cceb556e229787be7410b8ea70a5ea92ca694bd9
MayurRautCode <mayurraut016@gmail.com>
2024-05-29 15:18:25 +0530

    update 3

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,17 @@
-import React from "react";
-import NavbarC from "../component/navbarc";
-import Herosection from "../component/herosection";
-import Education from "../component/education";
-import Certificates from "../component/certification";
-import Skills from "../component/skills";
-import Project from "../component/project";
-import Profile from "../component/profile";
-import Contact from "../component/contact"; 
-import Footer from "../component/footer";
+import Navbarc from "./component/navbarc";
+import Herosection from "./component/herosection";
+import Education from "./component/education";
+import Certificates from "./component/certification";
+import Skills from "./component/skills";
+import Project from "./component/project";
+import Profile from "./component/profile";
+import Contact from "./component/contact"; 
+import Footer from "./component/footer";
 
 export default function Home() {
   return (
     <main className="bg-black min-h-screen flex flex-col scroll-smooth">
-      <NavbarC />
+      <Navbarc />
       <Herosection />
       <Education />
       <Certificates />
