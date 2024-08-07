1821c5b1c418d34d493a659dfe5e95ca7f2d8d02
MayurRautCode <mayurraut016@gmail.com>
2024-08-07 16:23:57 +0530

    update 11

--- src/app/component/skills.js
+++ src/app/component/skills.js
@@ -28,7 +28,7 @@ function Skills() {
           <button className='bg-white font-semibold text-sm md:text-lg lg:text-2xl rounded-full px-2 md:px-3 lg:px-5 py-2 lg:py-3 mr-2 md:mr-3 lg:mr-5 mb-6 flex items-center'> <Image src={figma} className='h-6 md:h-8 lg:h-10 w-6 md:w-8 lg:w-10'></Image> Figma</button>
           <button className='bg-white font-semibold text-sm md:text-lg lg:text-2xl rounded-full px-2 md:px-3 lg:px-5 py-2 lg:py-3 mr-2 md:mr-3 lg:mr-5 mb-6 flex items-center'> <Image src={github} className='h-6 md:h-8 lg:h-10 w-6 md:w-8 lg:w-10 mr-1'></Image> Github</button>
           <button className='bg-white font-semibold text-sm md:text-lg lg:text-2xl rounded-full px-2 md:px-3 lg:px-5 py-2 lg:py-3 mr-1 md:mr-3 lg:mr-5 mb-6 flex items-center'> <Image src={tailwind} className='h-6 md:h-8 lg:h-10 w-6 md:w-8 lg:w-10 md:mr-1'></Image> Tailwind CSS</button>
-          <button className='bg-white font-semibold text-sm md:text-lg lg:text-2xl rounded-full px-2 md:px-3 lg:px-5 py-2 lg:py-3 mr-2 md:mr-3 lg:mr-5 mb-6 flex items-center'> <Image src={vs} className='h-6 md:h-8 lg:h-10 w-6 md:w-8 lg:w-10 md:mr-1'></Image> Visual Studio Code</button>
+          <button className='bg-white font-semibold text-lg lg:text-2xl rounded-full px-2 md:px-3 lg:px-5 py-2 lg:py-3 mr-2 md:mr-3 lg:mr-5 mb-6 flex items-center'> <Image src={vs} className='h-6 md:h-8 lg:h-10 w-6 md:w-8 lg:w-10 md:mr-1'></Image> Visual Studio Code</button>
         </section>
     </div>
   )
